70e91621b5dddd66e6892328180d6872f3f686cf
paulaMarcella <paula.brinkhege@gmail.com>
2021-04-14 16:09:16 +0100

    added Card component

---
+++ components/layout/Card.js
@@ -0,0 +1,19 @@
+import styles from "../../styles/Card.module.scss";
+const Card = ({ src }) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.imgcontainer}>
+        <img src="./headers/bluesky-header.jpg" alt="platzhalter" />
+      </div>
+      <div className={styles.content}>
+        <h3 className={styles.title}>Card Beispiel</h3>
+        <p className={styles.text}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit deserunt
+          dicta delectus. A dolor, sequi quaerat natus dolore soluta autem?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Header from "../components/layout/Header";
 import Layout from "../components/layout/Layout";
+import Card from "../components/layout/Card";
 
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
@@ -20,8 +21,12 @@ const Home = () => {
             <h1>{t("home-intro-h1")}</h1>
             <p>{t("home-intro-p1")}</p>
             <p>{t("home-intro-p2")}</p>
+          </div>
+          <div className="container dark-bg">
+            <h1>Beispiele</h1>
             <button className="btn">Button Beispiel</button>
             <button className="btn outline">Button Outline</button>
+            <Card />
           </div>
         </section>
       </Layout>
