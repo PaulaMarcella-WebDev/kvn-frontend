5e346efe068fe5df0b1a6073b37b922e509314bc
paulaMarcella <paula.brinkhege@gmail.com>
2021-08-19 18:05:35 +0200

    up to date

--- pages/windkraft.js
+++ pages/windkraft.js
@@ -1,7 +1,24 @@
-import React from "react";
+import Head from "next/head";
+import Layout from "@/layout/Layout";
+import PageHeader from "@/layout/PageHeader";
 
-const windkraft = () => {
-  return <div></div>;
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
+const headerImg = "/headers/unternehmen.jpg";
+
+const About = () => {
+  const { t } = useTranslation(["services", "common"]);
+  return (
+    <>
+      <Layout metaTitle={t("services-title")}></Layout>
+    </>
+  );
 };
 
-export default windkraft;
+export default About;
+
+export const getStaticProps = async ({ locale }) => ({
+  props: {
+    ...(await serverSideTranslations(locale, ["about", "common"]))
+  }
+});
